5c8ec2c901aa02c01dfbff20f84e1c27fa9757ae
Te Thamrong <profte@gmail.com>
2015-06-05 05:54:40 -0400

    label functions

--- label_functions.py
+++ label_functions.py
@@ -1,3 +1,9 @@
+"""Label functions
+
+Each function should take a data_reader.DRelation object as an argument 
+and output a label string.
+
+"""
 
 def dimension_one(drelation):
 	senses = drelation.senses
