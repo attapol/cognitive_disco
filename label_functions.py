e2741c60d0811471b453e9e3d4d85391676b7708
irehbein <rehbein@coli.uni-sb.de>
2015-06-10 18:41:47 +0200

    commit modified label_functions.py

--- label_functions.py
+++ label_functions.py
@@ -4,6 +4,8 @@ Each function should take a data_reader.DRelation object as an argument
 and output a label string.
 
 """
+import json
+
 
 class LabelFunction(object):
 	
@@ -25,5 +27,75 @@ class OriginalLabel(LabelFunction):
 	def label(self, drelation):
 		senses = drelation.senses
 		return senses[0]
+
+
+
+""" read json object (mapping) from file
+    (not sure if we should keep that here)
+"""
+
+def read_json(file):
+        with open(file) as data_file:
+                mapping = json.load(data_file)
+
+        return mapping
+
+
+
+""" basic operation (additive/non-additive/causal/non-causal/n.a.)
+"""
+
+def dimension_one(drelation, mapping):
+        senses = drelation.senses
+        dimensions = []
+
+        for sense in senses:
+                dimensions.append(mapping[sense]['basic'])
+
+        return dimensions
+
+
+
+""" order (arg1 before arg2 => forward
+      or  arg2 before arg1  => backward
+      or  n.a.)
+"""
+
+def dimension_two(drelation, mapping):
+        senses = drelation.senses
+        dimensions = []
+
+        for sense in senses:
+                dimensions.append(mapping[sense]['order'])
+
+        return dimensions
+
+
+""" semantic / pragmatic (sem/prag/n.a.)
+"""
+
+def dimension_three(drelation):
+        senses = drelation.senses
+        dimensions = []
+
+        for sense in senses:
+                dimensions.append(mapping[sense]['sem_prag'])
+
+        return dimensions
+
+
+
+""" polarity (pos/neg/n.a.)
+"""
+
+def dimension_four(drelation):
+        senses = drelation.senses
+        dimensions = []
+
+        for sense in senses:
+                dimensions.append(mapping[sense]['polarity'])
+
+        return dimensions
+
 
 
